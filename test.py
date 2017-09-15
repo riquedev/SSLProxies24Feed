4fa51f938246ce224a4274362e198e6b9c24179f
Henrique da Silva Santos <rique_dev@hotmail.com>
2017-09-15 09:16:31 -0300

    :construction_worker:  Correção no test.py

--- test.py
+++ test.py
@@ -6,7 +6,6 @@
 from SSLProxies24.Feed import Feed
 from SSLProxies24.Check import CheckProxy
 import time
-import os
 import gc
 
 # Recupera a listagem
@@ -20,25 +19,15 @@ chk.validatelist(prx)
 
 # Ativa garbage
 gc.enable()
-while True:
 
-    # Limpa tela
-    os.system('cls || clear')
+time.sleep(30)
 
-    # Garbage
-    if gc.isenabled():
-        print('O Garbage Collector está ativo!')
+# Contagem
+print('Sucesso: '+str(chk.getsucesscount()))
+print('Falhas: '+str(chk.getfailcount()))
+print('Total de Proxys: '+str(chk.getproxycount()))
+print('Restam: '+str(chk.getproxycount()-(chk.getsucesscount()+chk.getfailcount())))
 
-    # Contagem
-    print('Sucesso: '+str(chk.getsucesscount()))
-    print('Falhas: '+str(chk.getfailcount()))
-    print('Total de Proxys: '+str(chk.getproxycount()))
-    print('Restam: '+str(chk.getproxycount()-(chk.getsucesscount()+chk.getfailcount())))
+# Lista de Proxys
+print(chk.getproxylist())
 
-    # Lista de Proxys
-    print(chk.getproxylist())
-
-    # Aguarda 5 segundos para atualizar dados
-    time.sleep(5)
-
-    break
